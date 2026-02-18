de4bead4c23994e35015df3be8838d8a3760cf69
rj8b0000 <100023483+rj8b0000@users.noreply.github.com>
2026-02-18 18:00:23 +0530

    C# practice - static keyword, method, inheritance, polymorhism concept, abstract class and methods

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4453f2bbabd8dc35472c9bf4fce463e1637cc405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d20e4e80738e0c13a015e4a6bae92aa307f2ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
